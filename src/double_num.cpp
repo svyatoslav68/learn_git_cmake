6b1873d98dbdc9cf3da464a59fdefbf6468524a8
svyatoslav68 <svjatoslav68@yandex.ru>
2020-01-04 14:21:37 +0300

    Add function sum() and module double_num.cpp

---
+++ src/double_num.cpp
@@ -0,0 +1,8 @@
+/* Module consist function for double number.
+   This functions for learn float number */
+
+double sum(const double x, const double y){
+	double result;
+	result = x + y;
+	return result;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,13 @@
 This file included function main().*/
 #include <iostream>
 
+double sum(const double, const double);
+
 int main(int argc, char **argv){
+	const double x = 1.345;
+	const double y = 10012.78762;
+	double result;
+	result = sum(x,y);
+	std::cout << "Сумма чисел " << x << " и " << y << " = " << result << std::endl;
 	return 0;
 }
